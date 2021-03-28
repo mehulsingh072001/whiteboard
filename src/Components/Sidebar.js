1242eb12c9607ed2a774e65e8b2aea49e643fc28
Mehul Singh Rathore <mehulsingh@gmail.com>
2021-03-28 19:06:49 +0530

    first commit

---
+++ src/Components/Sidebar.js
@@ -0,0 +1,16 @@
+import '../Styles/Sidebar.css'
+
+function Sidebar() {
+  return(
+    <div className="sidebar">
+      <nav>
+        <i class="fas fa-pencil-ruler"></i>
+        <i class="fas fa-palette"></i>
+        <i class="fas fa-eraser"></i>
+        <i class="fas fa-eraser"></i>
+      </nav>
+    </div>
+  )
+}
+
+export default Sidebar
