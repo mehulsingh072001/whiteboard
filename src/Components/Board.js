8ac87d1112b221e1a4b3923b53e7249996385237
Mehul Singh Rathore <mehulsingh@gmail.com>
2021-03-29 13:31:40 +0530

    did some work

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -1,13 +1,22 @@
+import React, { useState, useContext } from 'react'
+import { Context } from '../Context.js';
 import Draw from './Draw'
 import Erase from './Eraser'
 
+
 function Board() {
-  return(
-    <div>
-      <Draw/>
-      <Erase/>
-    </div>
-  )
+  const{ draw } = useContext(Context)
+
+    if(draw[0] === true) {
+      return(
+        <Draw/>
+      )
+    }
+    return(
+      <div>
+        <Erase/>
+      </div>
+    )
 }
 
 export default Board;
